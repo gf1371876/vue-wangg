a15cd74b65847d29d49b3048deba7d96888107bb
wanggang <911743828@qq.com>
2018-12-18 17:33:10 +0800

    增加dist

---
+++ dist/precache-manifest.a6b6ada134ead6d1ea53f7821a61d98e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "02f7600240d7726aa652",
+    "url": "/js/chunk-vendors.100419a4.js"
+  },
+  {
+    "revision": "1501541c4473b9ce5f4f",
+    "url": "/js/app.6ed06fe1.js"
+  },
+  {
+    "revision": "8871b0dd4d9fc9649819",
+    "url": "/js/about.a956700f.js"
+  },
+  {
+    "revision": "aa102d2b3e38033ef71eacc3e07ecb0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1501541c4473b9ce5f4f",
+    "url": "/css/app.44200696.css"
+  }
+];
